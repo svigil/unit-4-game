f91afdcf176d8f4c795b60eb2b16fe77813f6413
Spencer Vigil <spencervigil27@gmail.com>
2018-07-31 23:27:31 -0500

    To turn in before 12

---
+++ assets/java/game.js
@@ -0,0 +1,10 @@
+// Create necessary variables
+
+var userChoices = ["jack", "oogie", "sally", "santa", "zero"];
+    var enemyChoices = [];
+    var enemyDefeated = [];
+    
+    var player;
+    var currentEnemy;
+
+    console.log(userChoices);
